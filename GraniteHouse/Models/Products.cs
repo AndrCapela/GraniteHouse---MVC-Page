4e6aeecac623270e786c6f4e1d0bccdd7a062b71
André Capela <andr.capela@gmail.com>
2018-10-04 13:55:21 -0300

    CreateProductDatabase

---
+++ GraniteHouse/Models/Products.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GraniteHouse.Models
+{
+    public class Products
+    {
+        public int Id { get; set; }
+
+        public String Name { get; set; }
+
+        public double Price { get; set; }
+
+        public bool Avaliable { get; set; }
+
+        public string Image { get; set; }
+
+        public string ShadeColor { get; set; }
+
+        [Display(Name="Product Type")]
+        public int ProductTypeId { get; set; }
+
+        [ForeignKey("ProductTypeId")]
+        public int SpecialTagsID { get; set; }
+
+        [ForeignKey("SpecialTagsID")]
+        public virtual SpecialTags SpecialTags { get; set; }
+    }
+}
