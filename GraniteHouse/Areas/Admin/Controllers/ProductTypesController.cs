b60f5c0fde08262a5909feacab0e6df8e6d16749
André Capela <andr.capela@gmail.com>
2018-10-04 11:42:21 -0300

    EditProductType

--- GraniteHouse/Areas/Admin/Controllers/ProductTypesController.cs
+++ GraniteHouse/Areas/Admin/Controllers/ProductTypesController.cs
@@ -41,5 +41,39 @@ namespace GraniteHouse.Areas.Admin.Controllers
             }
             return View(producttypes);
         }
+
+        //Get Edit Action Method
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            var ProductType = await _db.ProductTypes.FindAsync(id);
+            if(ProductType == null)
+            {
+                return NotFound();
+            }
+
+            return View(ProductType);
+        }
+
+        //Post Edit action Method
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, ProductTypes producttypes)
+        {
+            if (id != producttypes.Id)
+            {
+                return NotFound();
+            }
+            if (ModelState.IsValid)
+            {
+                _db.Update(producttypes);
+                await _db.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(producttypes);
+        }
     }
 }
