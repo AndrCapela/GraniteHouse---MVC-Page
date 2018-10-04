21c745fe4f099c0f4abb8d35b5a6586ff98f29a2
André Capela <andr.capela@gmail.com>
2018-10-04 17:45:14 -0300

    Products - Create Action Method

--- GraniteHouse/Controllers/ProductsController.cs
+++ GraniteHouse/Controllers/ProductsController.cs
@@ -1,9 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using GraniteHouse.Data;
 using GraniteHouse.Models.ViewModels;
+using GraniteHouse.Utility;
+using Microsoft.AspNetCore.Hosting.Internal;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
@@ -12,12 +15,14 @@ namespace GraniteHouse.Controllers
     public class ProductsController : Controller
     {
         public readonly ApplicationDbContext _db;
+        public readonly HostingEnvironment _hostingEnvironment;
 
         [BindProperty]
         public ProductsViewModel ProductsVM { get; set; }
 
-        public ProductsController(ApplicationDbContext db)
+        public ProductsController(ApplicationDbContext db, HostingEnvironment hostingEnvironment)
         {
+            _hostingEnvironment = hostingEnvironment;
             _db = db;
             ProductsVM = new ProductsViewModel()
             {
@@ -31,6 +36,53 @@ namespace GraniteHouse.Controllers
         {
             var products = _db.Products.Include(m => m.SpecialTags).Include(m => m.ProductTypes);
             return View(await products.ToListAsync());
+        }
+
+        //Get : Products Create
+        public IActionResult Create()
+        {
+            return View(ProductsVM);
+        }
+
+        //Post : Product Create
+        [HttpPost, ActionName("Create")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> CreatePost()
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(ProductsVM);
+            }
+            _db.Products.Add(ProductsVM.Products);
+            await _db.SaveChangesAsync();
+
+            //Image being saved
+
+            string webRootPath = _hostingEnvironment.WebRootPath;
+            var files = HttpContext.Request.Form.Files;
+
+            var productsFromDb = _db.Products.Find(ProductsVM.Products.Id);
+
+            if(files.Count != 0)
+            {
+                //Image has been uploaded
+                var uploads = Path.Combine(webRootPath, SD.ImageFolder);
+                var extension = Path.GetExtension(files[0].FileName);
+
+                using (var filestream = new FileStream(Path.Combine(uploads, ProductsVM.Products.Id + extension),FileMode.Create))
+                {
+                    files[0].CopyTo(filestream);
+                }
+                productsFromDb.Image = @"\" + SD.ImageFolder + @"\" + ProductsVM.Products.Id + extension;
+            }
+            else
+            {
+                var uploads = Path.Combine(webRootPath, SD.ImageFolder + @"\" + SD.DefaultProductImage);
+                System.IO.File.Copy(uploads, webRootPath + @"\" + SD.ImageFolder + @"\" + ProductsVM.Products.Id + ".jpg");
+                productsFromDb.Image = (@"\"+SD.ImageFolder + @"\" + ProductsVM.Products.Id + ".jpg");
+            }
+            await _db.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
         }
     }
 }
