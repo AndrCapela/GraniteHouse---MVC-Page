8e7de2ea133ec211efbd0a75d97fabf5ae7c72d6
André Capela <andr.capela@gmail.com>
2018-10-05 16:23:30 -0300

    StartingShoppingCart

---
+++ GraniteHouse/Areas/Customer/Controllers/ShoppingCartController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using GraniteHouse.Data;
+using GraniteHouse.Extensions;
+using GraniteHouse.Models;
+using GraniteHouse.Models.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace GraniteHouse.Areas.Customer.Controllers
+{
+    [Area("Customer")]
+    public class ShoppingCartController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        [BindProperty]
+        public ShoppingCartViewModel ShoppingCartVM { get; set; }
+
+        public ShoppingCartController(ApplicationDbContext db)
+        {
+            _db = db;
+            ShoppingCartVM = new ShoppingCartViewModel()
+            {
+                Products = new List<Models.Products>()
+            };
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            List<int> listShoppingCart = HttpContext.Session.Get<List<int>>("ssShoppingCart");
+
+            if(listShoppingCart.Count > 0)
+            {
+                foreach(int cartItem in listShoppingCart)
+                {
+                    Products prod = _db.Products.Include(p =>p.SpecialTags).Include(p=> p.ProductTypes).Where(p => p.Id == cartItem).FirstOrDefault();
+                    ShoppingCartVM.Products.Add(prod);
+                }
+            }
+
+            return View(ShoppingCartVM);
+        }
+    }
+}
---
+++ GraniteHouse/Data/Migrations/20181005162055_addAppointmentsAndProductsSelectedForAppointment.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace GraniteHouse.Data.Migrations
+{
+    public partial class addAppointmentsAndProductsSelectedForAppointment : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Appointments",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    AppointmentDate = table.Column<DateTime>(nullable: false),
+                    CustomerName = table.Column<string>(nullable: true),
+                    CustomerPhoneNumber = table.Column<string>(nullable: true),
+                    CustomerEmail = table.Column<string>(nullable: true),
+                    isConfirmed = table.Column<bool>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Appointments", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ProductsSelectedForAppointment",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    AppointmentId = table.Column<int>(nullable: false),
+                    ProductId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ProductsSelectedForAppointment", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_ProductsSelectedForAppointment_Appointments_AppointmentId",
+                        column: x => x.AppointmentId,
+                        principalTable: "Appointments",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_ProductsSelectedForAppointment_Products_ProductId",
+                        column: x => x.ProductId,
+                        principalTable: "Products",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Products_ProductTypeId",
+                table: "Products",
+                column: "ProductTypeId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ProductsSelectedForAppointment_AppointmentId",
+                table: "ProductsSelectedForAppointment",
+                column: "AppointmentId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ProductsSelectedForAppointment_ProductId",
+                table: "ProductsSelectedForAppointment",
+                column: "ProductId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Products_ProductTypes_ProductTypeId",
+                table: "Products",
+                column: "ProductTypeId",
+                principalTable: "ProductTypes",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Products_ProductTypes_ProductTypeId",
+                table: "Products");
+
+            migrationBuilder.DropTable(
+                name: "ProductsSelectedForAppointment");
+
+            migrationBuilder.DropTable(
+                name: "Appointments");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Products_ProductTypeId",
+                table: "Products");
+        }
+    }
+}
