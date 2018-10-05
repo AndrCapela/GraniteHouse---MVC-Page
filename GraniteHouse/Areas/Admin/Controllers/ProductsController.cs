6ade8d66bb53e5a0dd15ee69c630aefba0514ace
André Capela <andr.capela@gmail.com>
2018-10-04 21:35:46 -0300

    EditAndDetailsProductsWorking

--- GraniteHouse/Areas/Admin/Controllers/ProductsController.cs
+++ GraniteHouse/Areas/Admin/Controllers/ProductsController.cs
@@ -11,30 +11,34 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 namespace GraniteHouse.Controllers
-{   [Area("Admin")]
+{
+    [Area("Admin")]
     public class ProductsController : Controller
     {
-        public readonly ApplicationDbContext _db;
-        public readonly HostingEnvironment _hostingEnvironment;
+        private readonly ApplicationDbContext _db;
+        private readonly HostingEnvironment _hostingEnvironment;
 
         [BindProperty]
         public ProductsViewModel ProductsVM { get; set; }
 
+
         public ProductsController(ApplicationDbContext db, HostingEnvironment hostingEnvironment)
         {
-            _hostingEnvironment = hostingEnvironment;
             _db = db;
+            _hostingEnvironment = hostingEnvironment;
             ProductsVM = new ProductsViewModel()
             {
                 ProductTypes = _db.ProductTypes.ToList(),
                 SpecialTags = _db.SpecialTags.ToList(),
                 Products = new Models.Products()
             };
+
         }
+
 
         public async Task<IActionResult> Index()
         {
-            var products = _db.Products.Include(m => m.SpecialTags).Include(m => m.ProductTypes);
+            var products = _db.Products.Include(m => m.ProductTypes).Include(m => m.SpecialTags);
             return View(await products.ToListAsync());
         }
 
@@ -44,15 +48,16 @@ namespace GraniteHouse.Controllers
             return View(ProductsVM);
         }
 
-        //Post : Product Create
+        //Post : Products Create
         [HttpPost, ActionName("Create")]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> CreatePost()
+        public async Task<IActionResult> CreatePOST()
         {
             if (!ModelState.IsValid)
             {
                 return View(ProductsVM);
             }
+
             _db.Products.Add(ProductsVM.Products);
             await _db.SaveChangesAsync();
 
@@ -63,13 +68,13 @@ namespace GraniteHouse.Controllers
 
             var productsFromDb = _db.Products.Find(ProductsVM.Products.Id);
 
-            if(files.Count != 0)
+            if (files.Count != 0)
             {
                 //Image has been uploaded
                 var uploads = Path.Combine(webRootPath, SD.ImageFolder);
                 var extension = Path.GetExtension(files[0].FileName);
 
-                using (var filestream = new FileStream(Path.Combine(uploads, ProductsVM.Products.Id + extension),FileMode.Create))
+                using (var filestream = new FileStream(Path.Combine(uploads, ProductsVM.Products.Id + extension), FileMode.Create))
                 {
                     files[0].CopyTo(filestream);
                 }
@@ -77,30 +82,104 @@ namespace GraniteHouse.Controllers
             }
             else
             {
+                //when user does not upload image
                 var uploads = Path.Combine(webRootPath, SD.ImageFolder + @"\" + SD.DefaultProductImage);
-                System.IO.File.Copy(uploads, webRootPath + @"\" + SD.ImageFolder + @"\" + ProductsVM.Products.Id + ".jpg");
-                productsFromDb.Image = (@"\"+SD.ImageFolder + @"\" + ProductsVM.Products.Id + ".jpg");
+                System.IO.File.Copy(uploads, webRootPath + @"\" + SD.ImageFolder + @"\" + ProductsVM.Products.Id + ".png");
+                productsFromDb.Image = @"\" + SD.ImageFolder + @"\" + ProductsVM.Products.Id + ".png";
             }
             await _db.SaveChangesAsync();
+
             return RedirectToAction(nameof(Index));
+
         }
 
-        //Get : Edit
 
+
+        //GET : Edit
         public async Task<IActionResult> Edit(int? id)
         {
             if (id == null)
             {
                 return NotFound();
             }
+
             ProductsVM.Products = await _db.Products.Include(m => m.SpecialTags).Include(m => m.ProductTypes).SingleOrDefaultAsync(m => m.Id == id);
 
             if (ProductsVM.Products == null)
             {
                 return NotFound();
             }
-            return View(ProductsVM);
 
+            return View(ProductsVM);
         }
+
+
+        //Post : Edit
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id)
+        {
+            if (ModelState.IsValid)
+            {
+                string webRootPath = _hostingEnvironment.WebRootPath;
+                var files = HttpContext.Request.Form.Files;
+
+                var productFromDb = _db.Products.Where(m => m.Id == ProductsVM.Products.Id).FirstOrDefault();
+                
+                if (files.Count > 0 && files[0]!=null)
+                {
+                    //if user uploads a new image
+                    var uploads = Path.Combine(webRootPath, SD.ImageFolder);
+                    var extension_new = Path.GetExtension(files[0].FileName).ToUpper();
+                    var extension_old = Path.GetExtension(productFromDb.Image);
+
+                    if (System.IO.File.Exists(Path.Combine(uploads, ProductsVM.Products.Id + extension_old)))
+                    {
+                        System.IO.File.Delete(Path.Combine(uploads, ProductsVM.Products.Id + extension_old));
+                    }
+                    using (var filestream = new FileStream(Path.Combine(uploads, ProductsVM.Products.Id + extension_new), FileMode.Create))
+                    {
+                        files[0].CopyTo(filestream);
+                    }
+                    ProductsVM.Products.Image = @"\" + SD.ImageFolder + @"\" + ProductsVM.Products.Id + extension_new;
+                }
+
+                if (ProductsVM.Products.Image != null)
+                {
+                    productFromDb.Image = ProductsVM.Products.Image;
+                }
+
+                productFromDb.Name = ProductsVM.Products.Name;
+                productFromDb.Price = ProductsVM.Products.Price;
+                productFromDb.Avaliable = ProductsVM.Products.Avaliable;
+                productFromDb.ProductTypeId = ProductsVM.Products.ProductTypeId;
+                productFromDb.SpecialTagsID = ProductsVM.Products.SpecialTagsID;
+                productFromDb.ShadeColor = ProductsVM.Products.ShadeColor;
+                await _db.SaveChangesAsync();
+
+                return RedirectToAction(nameof(Index));
+            }
+
+            return View(ProductsVM);
+        }
+
+        //GET : Details
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            ProductsVM.Products = await _db.Products.Include(m => m.SpecialTags).Include(m => m.ProductTypes).SingleOrDefaultAsync(m => m.Id == id);
+
+            if (ProductsVM.Products == null)
+            {
+                return NotFound();
+            }
+
+            return View(ProductsVM);
+        }
+
     }
 }
